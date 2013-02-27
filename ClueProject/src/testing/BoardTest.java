ec45c8a0f3f689fb678d70d6f3077aed6c05786c
John <john@john-MacBookPro.(none)>
2013-02-27 12:10:37 -0700

    actually added Legend.csv

--- ClueProject/src/testing/BoardTest.java
+++ ClueProject/src/testing/BoardTest.java
@@ -18,16 +18,21 @@ import utilities.RoomCell;
 
 public class BoardTest {
 	private Board testBoard;
-	//set up by initializing the board with a default constructor and telling it to load some files.
+	//set up by initializing the board with a default constructor and telling it to
+	//load the proper config files
 	@Before
 	public void setup() {
 		testBoard = new Board("Board.csv", "Legend.csv");
 		testBoard.loadConfigFiles();
 	}
+	//Test to make sure that we have the right legend, 
+	//which should include the 9 rooms, a closet, and a walkway.
 	@Test
 	public void testCorrectNumberRooms() {
 		Assert.assertEquals(11, testBoard.getRooms().size());
 	}
+	//Second test for the legend - make sure that the legend
+	//mapped the right keys to the correct strings.
 	@Test
 	public void testRoomMapping() {
 		Assert.assertEquals("Conservatory", testBoard.getRooms().get('C'));
@@ -42,22 +47,28 @@ public class BoardTest {
 		Assert.assertEquals("Hall", testBoard.getRooms().get('H'));
 		Assert.assertEquals("Closet", testBoard.getRooms().get('X'));
 	}
+	
+	//make sure that the Board file was read correctly, i.e. it had the correct
+	//number of rows and columns.
 	@Test
 	public void testRowsAndColumns() {
 		Assert.assertEquals(25, testBoard.getNumRows());
 		Assert.assertEquals(25, testBoard.getNumColumns());
 	}
+	//make sure that the doors appeared in the correct locations and open
+	//in the correct direction after loading.
 	@Test
 	public void testDoors() {
 		Assert.assertEquals(RoomCell.DoorDirection.DOWN, testBoard.getRoomCellAt(6, 3));
 		Assert.assertEquals(RoomCell.DoorDirection.UP, testBoard.getRoomCellAt(20,18));
 		Assert.assertEquals(RoomCell.DoorDirection.RIGHT, testBoard.getRoomCellAt(19, 4));
 		Assert.assertEquals(RoomCell.DoorDirection.LEFT, testBoard.getRoomCellAt(11, 19));
-		
+		//also make sure that things that _aren't_ supposed to be doors aren't.
 		//room
 		Assert.assertEquals(false, testBoard.getRoomCellAt(0, 0).isDoorway());
 		//walkway
 		Assert.assertEquals(false, testBoard.getRoomCellAt(8, 6).isDoorway());
+		//count up the doors.
 		int counter=0;
 		for(BoardCell b: testBoard.getCells()) {
 			if(b.isDoorway()) {
@@ -66,6 +77,7 @@ public class BoardTest {
 		}
 		Assert.assertEquals(19, counter);
 	}
+	//Select some random spots from rooms and ensure that they mapped correctly.
 	@Test
 	public void testRooms() {
 		Assert.assertEquals('S', testBoard.getRoomCellAt(0, 0).getRoomInitial());
@@ -74,12 +86,14 @@ public class BoardTest {
 		Assert.assertEquals('D', testBoard.getRoomCellAt(12, 22).getRoomInitial());
 		
 	}
+	//test calcIndex, pretty straightforward, make sure it's outputting no_rows*row + column.
 	@Test
 	public void testCalcIndex() {
 		Assert.assertEquals(0, testBoard.calcIndex(0, 0));
 		Assert.assertEquals(624,  testBoard.calcIndex(24, 24));
 		Assert.assertEquals(312, testBoard.calcIndex(12, 12));
 	}
+	//test for exception throwing, given a bad legend .csv file.
 	@Test (expected = BadConfigFormatException.class)
 	public void testExceptions() throws BadConfigFormatException, FileNotFoundException {
 		Board b = new Board("Board.csv", "BadLegend.csv");
--- ClueProject/src/utilities/Board.java
+++ ClueProject/src/utilities/Board.java
@@ -1,5 +1,6 @@
 package utilities;
 
+//Board class, contains the legend (cells) and the actual rooms.
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -9,13 +10,16 @@ public class Board {
 	private int numRows;
 	private int numColumns;
 	
+	//default = empty lists/sets + no columns and rows.
 	public Board() {
 		this.cells = new ArrayList<BoardCell>();
 		this.rooms = new HashMap<Character, String>();
 		this.numRows = 0;
 		this.numColumns = 0;
 	}
-	public Board(String string, String string2) {
+	//todo: add member strings for board and legend csv files?
+	//not needed for failing tests.
+	public Board(String boardCsv, String legendCsv) {
 		
 	}
 	public void loadConfigFiles() {
@@ -24,6 +28,8 @@ public class Board {
 	public int calcIndex(int row, int column) {
 		return 0;
 	}
+	
+	//getters.
 	public RoomCell getRoomCellAt(int row, int column) {
 		return new RoomCell();
 	}
